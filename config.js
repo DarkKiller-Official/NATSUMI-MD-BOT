b38ac98c519c402229e96173df9cf54d2c001de7
✰✶  DARK KILLER ✰✶ <99461944+DarkKiller-Official@users.noreply.github.com>
2022-06-04 08:02:10 +0530

    👑👑

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
+global.owner = ['+94761209144'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
